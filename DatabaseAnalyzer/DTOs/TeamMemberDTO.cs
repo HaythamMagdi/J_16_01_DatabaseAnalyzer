cde90eb84bbf869085f0182fec01e6a44b411b91
Haytham Magdi <haytham.magdi@hindawi.local>
2017-06-11 12:58:33 +0200

    first cmt

---
+++ DatabaseAnalyzer/DTOs/TeamMemberDTO.cs
@@ -0,0 +1,112 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using BD.DomainModel.DataTransferObjects;
+
+
+namespace BD.DomainModel.DataTransferObjects
+{
+    public class TeamMemberDTO
+    {
+        public int TeamMemberId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public String Position { get; set; }
+        public String Team { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Boolean Active { get; set; }
+        public Boolean IsConcurrentAccount { get; set; }
+        public DateTime OriginalEndDate { get; set; }
+
+    }
+}
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+    -- Create Table Script
+
+    CREATE TABLE [dbo].[Tbl_TeamMemberDTO](    
+        [TeamMemberId] int,
+[FirstName] [nvarchar](300),
+[LastName] [nvarchar](300),
+[Password] [nvarchar](300),
+[Email] [nvarchar](300),
+[Position] [nvarchar](300),
+[Team] [nvarchar](300),
+[StartDate] datetime,
+[EndDate] datetime,
+[Active] boolean,
+[IsConcurrentAccount] boolean,
+[OriginalEndDate] datetime
+
+    )
+
+
+*/
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+namespace BD.DomainModel.DataTransferObjectManagers
+{
+
+    public class TeamMemberDTOMgr
+    {
+
+        public static TeamMemberDTO FromDataRow(DataRow dr)
+        {
+            TeamMemberDTO res = new TeamMemberDTO();
+
+            DataTable table = dr.Table;
+
+            foreach (DataColumn col in table.Columns)
+            {
+                res.TeamMemberId = (int)dr["TeamMemberId"];
+                res.FirstName = (String)dr["FirstName"];
+                res.LastName = (String)dr["LastName"];
+                res.Password = (String)dr["Password"];
+                res.Email = (String)dr["Email"];
+                res.Position = (String)dr["Position"];
+                res.Team = (String)dr["Team"];
+                res.StartDate = (DateTime)dr["StartDate"];
+                res.EndDate = (DateTime)dr["EndDate"];
+                res.Active = (Boolean)dr["Active"];
+                res.IsConcurrentAccount = (Boolean)dr["IsConcurrentAccount"];
+                res.OriginalEndDate = (DateTime)dr["OriginalEndDate"];
+
+            }
+
+            return res;
+        }
+
+        public static List<TeamMemberDTO> CreateListFromDataTable(DataTable table)
+        {
+            List<TeamMemberDTO> list_DTOs = new List<TeamMemberDTO>();
+
+            foreach (DataRow dr in table.Rows)
+            {
+                list_DTOs.Add(TeamMemberDTOMgr.FromDataRow(dr));
+            }
+
+            return list_DTOs;
+        }
+
+    }
+
+}
+
---
+++ DatabaseAnalyzer/Util/DTOStringMaker.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace DatabaseAnalyzer.Util
+{
+    public class DTOStringMaker
+    {
+        class DTOProperty
+        {
+            public string Name { get; set; }
+            public string TableColName { get; set; }
+            public string TypeName { get; set; }
+            public string SqlTypeName { get; set; }
+        }
+
+
+        public static string MakeDTOSring(string dtoClassName, DataTable table)
+        {
+            List<DTOProperty> list_Props = new List<DTOProperty>();
+
+            foreach (DataColumn col in table.Columns)
+            {
+                DTOProperty prop = new DTOProperty
+                {
+                    //Name = col.ColumnName.Trim().Replace(" ", "_"),
+                    Name = col.ColumnName.Trim().Replace(" ", ""),
+                    TableColName = col.ColumnName,
+
+                    TypeName = col.DataType.Name
+                        .Replace("Int64", "long")
+                        .Replace("Int32", "int"),
+
+                    SqlTypeName = col.DataType.Name.ToLower()
+                        .Replace("int64", "bigint")
+                        .Replace("int32", "int")
+                        .Replace("string", "[nvarchar](300)"),
+
+                };
+                list_Props.Add(prop);
+            }
+
+
+
+            string dtoFileTemplate = @"
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using BD.DomainModel.DataTransferObjects;
+
+
+namespace BD.DomainModel.DataTransferObjects
+{
+    public class <^DTOClassName^>
+    {
+        <^DEF_PROPS^>
+    }
+}
+
+
+                
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+    -- Create Table Script
+
+    CREATE TABLE [dbo].[Tbl_<^DTOClassName^>](    
+        <^TableColDef^>
+    )
+
+
+*/
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+namespace BD.DomainModel.DataTransferObjectManagers
+{
+
+    public class <^DTOClassName^>Mgr
+    {
+
+        public static <^DTOClassName^> FromDataRow(DataRow dr)
+        {
+            <^DTOClassName^> res = new <^DTOClassName^>();
+
+            DataTable table = dr.Table;
+
+            foreach (DataColumn col in table.Columns)
+            {
+                <^COPY_PROPS_FROM_ROW^>                       
+            }
+
+            return res;
+        }
+
+        public static List<<^DTOClassName^>> CreateListFromDataTable(DataTable table)
+        {
+            List<<^DTOClassName^>> list_DTOs = new List<<^DTOClassName^>>();
+
+            foreach (DataRow dr in table.Rows)
+            {
+                list_DTOs.Add(<^DTOClassName^>Mgr.FromDataRow(dr));
+            }
+
+            return list_DTOs;
+        }
+
+    }
+
+}
+
+                ";
+            string dtoFileStr = dtoFileTemplate.Replace("<^DTOClassName^>", dtoClassName);
+
+            {
+                string tableColDef = "";
+                for (int i = 0; i < list_Props.Count; i++)
+                {
+                    DTOProperty prop = list_Props[i];
+                    //tableColDef += "[" + prop.TableColName + "] " + prop.SqlTypeName + "^CMA^" + "\r\n";
+                    tableColDef += "[" + prop.Name + "] " + prop.SqlTypeName + "^CMA^" + "\r\n";
+
+                    if (i < list_Props.Count - 1)
+                    {
+                        tableColDef = tableColDef.Replace("^CMA^", ",");
+                    }
+                    else
+                    {
+                        tableColDef = tableColDef.Replace("^CMA^", "");
+                    }
+                }
+                dtoFileStr = dtoFileStr.Replace("<^TableColDef^>", tableColDef);
+            }
+
+            {
+                string propDef = "";
+                foreach (DTOProperty prop in list_Props)
+                {
+                    propDef += "public " + prop.TypeName + " " + prop.Name + " { get; set; }" + "\r\n";
+                }
+                dtoFileStr = dtoFileStr.Replace("<^DEF_PROPS^>", propDef);
+            }
+
+            {
+                string propFromColsStr = "";
+                foreach (DTOProperty prop in list_Props)
+                {
+                    propFromColsStr += "res." + prop.Name + " = (" + prop.TypeName + ")dr[\"" + prop.TableColName + "\"];" + "\r\n";
+                }
+                dtoFileStr = dtoFileStr.Replace("<^COPY_PROPS_FROM_ROW^>", propFromColsStr);
+            }
+
+
+            return dtoFileStr;
+        }
+
+
+
+
+    }
+}
