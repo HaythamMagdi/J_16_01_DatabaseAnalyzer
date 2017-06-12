4eeb7713f0728d261416eb34bb550f5f0aeb3b43
Haytham Magdi <haytham.magdi@hindawi.local>
2017-06-12 13:01:17 +0200

    Loaded multiple tables from a datareader

---
+++ DatabaseAnalyzer/DTOs/ProductDTO.cs
@@ -0,0 +1,85 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using BD.DomainModel.DataTransferObjects;
+
+
+namespace BD.DomainModel.DataTransferObjects
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal UnitPrice { get; set; }
+
+    }
+}
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+    -- Create Table Script
+
+    CREATE TABLE [dbo].[Tbl_ProductDTO](    
+        [Id] int,
+[Name] [nvarchar](300),
+[UnitPrice] decimal
+
+    )
+
+
+*/
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+namespace BD.DomainModel.DataTransferObjectManagers
+{
+
+    public class ProductDTOMgr
+    {
+
+        public static ProductDTO FromDataRow(DataRow dr)
+        {
+            ProductDTO res = new ProductDTO();
+
+            DataTable table = dr.Table;
+
+            foreach (DataColumn col in table.Columns)
+            {
+                res.Id = (int)dr["Id"];
+                res.Name = (string)dr["Name"];
+                res.UnitPrice = (Decimal)dr["UnitPrice"];
+
+            }
+
+            return res;
+        }
+
+        public static List<ProductDTO> CreateListFromDataTable(DataTable table)
+        {
+            List<ProductDTO> list_DTOs = new List<ProductDTO>();
+
+            foreach (DataRow dr in table.Rows)
+            {
+                list_DTOs.Add(ProductDTOMgr.FromDataRow(dr));
+            }
+
+            return list_DTOs;
+        }
+
+    }
+
+}
+
---
+++ DatabaseAnalyzer/Try1.cs
@@ -0,0 +1,74 @@
+﻿using BD.DomainModel.DataTransferObjectManagers;
+using DatabaseAnalyzer.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAnalyzer
+{
+    public class Try1
+    {
+        public static void Proceed()
+        {
+
+            //const string dbCncStr = "Data Source=sql2.bds.hindawi.com,32000;Initial Catalog=BDS_20170220;Persist Security Info=True;User ID=sa;Password=BDS@2020";
+            const string dbCncStr = "Data Source=.;Initial Catalog=OnlineStoreDB;Integrated Security=True";
+
+            var table1 = new DataTable();
+            var dataSet1 = new DataSet();
+
+            using (var conn = new SqlConnection(dbCncStr))
+            {
+                conn.Open();
+
+                //var cmd1 = new SqlCommand("select * from TeamMembers", conn);
+                //var cmd1 = new SqlCommand("select * from GeoGraphJournalsDistributionReportCache", conn);
+
+                //var cmd1 = new SqlCommand("SET FMTONLY ON; select * from GeoGraphJournalsDistributionReportCache; SET FMTONLY OFF", conn);
+                //var cmd1 = new SqlCommand("select * from GeoGraphJournalsDistributionReportCache", conn);
+
+                //var cmd1 = new SqlCommand("exec proc_LondonCampaign_GetStatuses", conn);
+
+                var cmd1 = new SqlCommand("select * from Products", conn);
+
+                                
+
+                SqlDataReader reader = cmd1.ExecuteReader();
+
+
+                var schemaTable = reader.GetSchemaTable();
+                string schmDtoStr = DTOStringMaker.MakeDTOSring("SchemaTableDTO", schemaTable);
+                var list_schemaTableDTOs = SchemaTableDTOMgr.CreateListFromDataTable(schemaTable);
+
+
+                //var schmCols = reader.GetSchemaTable().Columns;
+                //var rows = reader.GetSchemaTable().Rows;
+                //var row0 = reader.GetSchemaTable().Rows[0];
+
+                //SqlDbType type = (SqlDbType)(int)reader.GetSchemaTable().Rows[0]["ProviderType"];
+
+                table1.Load(reader);
+                //dataSet1.Load(reader,);
+
+
+                //var adapt1 = new SqlDataAdapter(cmd1);
+
+                //adapt1.Fill(table1);
+
+                //string dtoStr = DTOStringMaker.MakeDTOSring("TeamMemberDTO", table1);
+                //string dtoStr = DTOStringMaker.MakeDTOSring("GeoGraphJournalsDistributionReportCacheDTO", table1);
+                string dtoStr = DTOStringMaker.MakeDTOSring("ProductDTO", table1);
+
+                //var list_TeamMembers = TeamMemberDTOMgr.CreateListFromDataTable(table1);
+                var list_Products = ProductDTOMgr.CreateListFromDataTable(table1);
+            }
+
+
+
+        }
+    }
+}
